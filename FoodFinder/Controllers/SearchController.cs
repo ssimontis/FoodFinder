8902e620933a7ca5d4cd6103c324b8b1274e9490
Scott Simontis <ssimontis@gmail.com>
2022-06-08 20:21:20 -0400

    Adding Docker compose for Redis storage

---
+++ FoodFinder/Controllers/SearchController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodFinder.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class SearchController : ControllerBase {
+    private readonly ILogger<SearchController> _logger;
+
+    public SearchController(ILogger<SearchController> logger)
+    {
+      _logger = logger;
+    }
+  }
+}
